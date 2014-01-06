7687191c20097f2b830f61d971a004752aa326a0
Stefan Wallin <github@stefan-wallin.se>
2014-01-06 10:35:03 +0100

    Modeled the basic memory card.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,44 @@
-console.log('\'Allo \'Allo!');
+if(ko === undefined) {
+    throw "KnockoutJS not properly loaded!";
+}
+
+function cardViewModel() {
+	'use strict';
+	var self = this;
+
+    self.flipped = ko.observable(false);
+    self.front = ko.observable('front');
+    self.back = ko.observable('back');
+    self.className = ko.computed(function() {
+        if(self.flipped()){
+            return 'card flipped';
+        } else {
+            return 'card';
+        }
+    });
+    self.flip = function() {
+        console.log('flipping', self.flipped());
+        if(self.flipped()){
+            self.flipped(false);
+        } else {
+            self.flipped(true);
+        }
+    };
+
+}
+
+function AppViewModel() {
+	'use strict';
+	var self = this;
+	self.result = 0;
+	self.cards = ko.observableArray([]);
+	self.addCard = function (front, back) {
+        var card = new cardViewModel();
+        card.front(front);
+        card.back(back);
+		self.cards.push(card);
+	};
+}
+var app = new AppViewModel();
+ko.applyBindings( app );
+app.addCard('test1front', 'test2back');
